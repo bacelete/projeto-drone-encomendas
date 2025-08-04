bfc67e04fffb6bf818e3ae95434e9e6897f9adb2
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-08-04 11:22:33 -0300

    add backend com spring boot
    dependencias instaladas:
    - lombok
    - jpa
    - spring web
    - spring dev tools

---
+++ backend/encomendas/encomendas/src/test/java/com/dti/encomendas/EncomendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dti.encomendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncomendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
