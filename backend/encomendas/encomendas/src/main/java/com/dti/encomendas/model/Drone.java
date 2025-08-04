c144195d9966cc750159e39fe306bd4286a9265c
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-08-04 11:44:40 -0300

    atributos drone + annotations para os models

--- backend/encomendas/encomendas/src/main/java/com/dti/encomendas/model/Drone.java
+++ backend/encomendas/encomendas/src/main/java/com/dti/encomendas/model/Drone.java
@@ -1,4 +1,31 @@
 package com.dti.encomendas.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Drone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double pesoMax;
+    private double kmMax;
+
+    @Min(value = 0)
+    @Max(value = 100)
+    private int bateria;
+
+    @Enumerated(EnumType.STRING)
+    private StatusDrone status;
+
+    public Drone() {}
+
 }
--- backend/encomendas/encomendas/src/main/java/com/dti/encomendas/model/Pedido.java
+++ backend/encomendas/encomendas/src/main/java/com/dti/encomendas/model/Pedido.java
@@ -1,4 +1,14 @@
 package com.dti.encomendas.model;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
 public class Pedido {
+
 }
