de50a3b0b70da84580b6fe905f963e63e50c9eec
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-08-04 16:15:10 -0300

    custom error message + status e @RestControllerAdvice class

---
+++ backend/encomendas/encomendas/src/main/java/com/dti/encomendas/exception/RestExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.dti.encomendas.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(value = NotFoundException.class)
+    public ResponseEntity<ErrorMessage> notFoundException(NotFoundException exception) {
+      ErrorMessage errorMessage = new ErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMessage);
+    }
+}
