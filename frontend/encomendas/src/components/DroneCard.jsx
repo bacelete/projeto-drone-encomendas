2d0cc592bfa1b8f882eef0335a0be339a2aa39da
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-09-01 14:54:13 -0300

    feat: initial template of drone cards!

--- frontend/encomendas/src/components/DroneCard.jsx
+++ frontend/encomendas/src/components/DroneCard.jsx
@@ -1,7 +1,18 @@
-export default function DroneCard() {
+import DroneIcon from '../assets/icons/drone.png';
+
+export default function DroneCard({bateria, status}) {
     return (
-        <div>
-            <img src='../assets/icons/drone'/>
+        <div className='bg-white p-2 rounded-md flex gap-3 text-gray-800 w-lg'>
+            <img 
+                src={DroneIcon} 
+                alt="Drone Icon" 
+                className="w-12 h-12 object-contain" 
+            />
+            <div id="infos" className='p-2'>
+                <p id='titulo' className='font-bold text-xl'>Drone 1</p>
+                <p>Bateria: </p>
+                <p>Status: </p>
+            </div>
         </div>
     )
 }
--- frontend/encomendas/src/pages/DroneDashboard.jsx
+++ frontend/encomendas/src/pages/DroneDashboard.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react"
 import DroneCard from "../components/DroneCard"
 export default function DroneDashboard() {
+    const [drones, setDrones] = useState([]);
+    useEffect(() => {
+        async function fetchDrones() {
+            try {
+                const response = await fetch(`http://localhost:8080/drones/status`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                })
+                if (response.ok) {
+                    const drones = response.json();
+                    setDrones(drones);
+                }
+            }
+            catch (e) {
+                console.log(e);
+            }
+        }
+        fetchDrones();
+    }, []);
+
     return (
         <div>
-            <DroneCard />
+            {drones.map(drone => <DroneCard bateria={drone.bateria} status={drone.statusDrone} />)}
         </div>
     )
 }
