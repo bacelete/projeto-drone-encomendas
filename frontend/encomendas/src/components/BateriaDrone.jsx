7d2bb4b43b4a9bc3d7ac02797b42da63b1cd3957
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-09-02 12:33:00 -0300

    feat: adding animated battery low levels

--- frontend/encomendas/src/components/BateriaDrone.jsx
+++ frontend/encomendas/src/components/BateriaDrone.jsx
@@ -1,9 +1,20 @@
-import BatteryIcon from '../assets/icons/battery.png'
+import FullBattery from '../assets/icons/full-battery.png'
+import HalfBattery from '../assets/icons/half-battery.png'
+import Battery from '../assets/icons/battery.png'
+import LowBattery from '../assets/icons/low-battery.png'
+import EmptyBattery from '../assets/icons/empty-battery.png'
 
 export default function BateriaDrone({ battery }) {
+    let BatteryIcon; 
+    if (battery == 100) { BatteryIcon = FullBattery; }
+    if (battery > 50 && battery <= 100) { BatteryIcon = Battery; }
+    if (battery == 50) { BatteryIcon = HalfBattery; }
+    if (battery < 50 && battery <= 100) { BatteryIcon = LowBattery; }
+    if (battery == 0) { BatteryIcon = EmptyBattery; }
+
     return <>
         <div className='flex gap-2'>
-            <img src={BatteryIcon} alt="" />
+            <img src={BatteryIcon} className="w-5" alt="" />
             <p>Bateria: {battery}%</p>
         </div>
     </>
--- frontend/encomendas/src/components/ReloadButton.jsx
+++ frontend/encomendas/src/components/ReloadButton.jsx
@@ -7,7 +7,7 @@ export default function ReloadButton() {
 
     return (
         <button type="button" 
-        className="bg-sky-500 font-bold hover:bg-sky-600 
+        className="bg-sky-500 font-bold hover:bg-sky-600 transition hover:-translate-y-1 hover:scale-100
         hover:border-sky-500 border-3 border-sky-400 w-xs flex gap-2 my-5 
         text-white rounded-lg p-2 align-center cursor-pointer duration-300 ease-in-out" 
         onClick={handleRefresh}>
