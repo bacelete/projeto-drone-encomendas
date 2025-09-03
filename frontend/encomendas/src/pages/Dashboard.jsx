fa800bc919fab95d1cab92d666cd13d596457562
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-09-03 16:04:37 -0300

    style: more icons + style modal

--- frontend/encomendas/src/pages/Dashboard.jsx
+++ frontend/encomendas/src/pages/Dashboard.jsx
@@ -3,10 +3,12 @@ import DroneCard from "../components/DroneCard"
 import Title from "../components/Title";
 import ReloadButton from '../components/ReloadButton';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import BateriaDrone from "../components/BateriaDrone";
+import InfoIcon from '../assets/icons/info.png'
+import OrderIcon from '../assets/icons/order.png'
+import StatusDrone from "../components/StatusDrone";
 
 export default function Dashboard() {
     const [drones, setDrones] = useState([]);
@@ -93,28 +95,41 @@ export default function Dashboard() {
                         left: '50%',
                         transform: 'translate(-50%, -50%)',
                         width: 800,
-                        height: 400,
+                        height: 600,
                         bgcolor: 'background.paper',
                         border: '2px solid #000',
                         boxShadow: 24,
                         p: 5,
                         borderRadius: 2
                     }}>
-                        <div id="drone-infos">
-                            <Typography id="modal-modal-title" variant="h4" component="h2">
-                                Informações do Drone
-                            </Typography>
-                            <div className="infos p-2">
-                                <BateriaDrone battery={infoDrone.bateria} />
-                                <p className="text-lg">Alcance máximo: {infoDrone.kmMax} km</p>
-                                <p className="text-lg">Peso máximo suportado: {infoDrone.pesoMax} kg</p>
-                            </div>
+
+                        {/* Cabeçalho do Modal */}
+                    <div className="flex justify-between items-center mb-4">
+                        <Typography id="modal-title" variant="h4" component="h2" className="flex items-center gap-3 font-oxygen">
+                            <img src={InfoIcon} className="w-8 h-8" alt="Info Icon" />
+                            <span className="font-oxygen">Drone #{infoDrone.id}</span>
+                        </Typography>
+                        <button onClick={handleClose} className="text-gray-500 hover:text-gray-800 text-2xl">&times;</button>
+                    </div>
+
+                         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-4 bg-slate-50 rounded-lg mb-6">
+                        <div>
+                            <h3 className="text-xl font-semibold text-gray-700 mb-3 border-b pb-2">Status e Energia</h3>
+                            <BateriaDrone battery={infoDrone.bateria} />
+                            <StatusDrone status={infoDrone.status} />
                         </div>
-                        <div id="drone-pedidos" className="mt-3">
-                            <Typography variant="h4" component="h2">
-                                Pedidos
+                        <div className="p-1">
+                            <h3 className="text-xl font-semibold text-gray-700 mb-3 border-b pb-2">Capacidades</h3>
+                            <p className="text-lg text-gray-600 mt-3"><strong>Alcance máximo:</strong> {infoDrone.kmMax} km</p>
+                            <p className="text-lg text-gray-600"><strong>Peso máximo:</strong> {infoDrone.pesoMax} kg</p>
+                        </div>
+                    </div>
+                        <div id="drone-pedidos" className="my-3">
+                            <Typography variant="h4" component="h2" className="flex items-center gap-3 mb-7 font-oxygen">
+                                <img src={OrderIcon} className="w-8 h-8" alt="" />
+                                <span className="font-oxygen">Pedidos</span>
                             </Typography>
-                            <div className="infos p-2">
+                            <div className="infos max-h-48 overflow-y-auto bg-slate-50 rounded-lg p-3 my-5">
                                 {infoDrone.pedidos && infoDrone.pedidos.length > 0 ? (
                                     <ul>
                                         {infoDrone.pedidos.map((pedido) => (
@@ -122,7 +137,7 @@ export default function Dashboard() {
                                                 <p className="text-2xl"><strong>ID: {pedido.id}</strong></p>
                                                 <div className="mx-3">
                                                     <p className="text-lg"><strong>Peso:</strong> {pedido.peso} kg</p>
-                                                    <p className="text-lg"><strong>Localização: </strong>
+                                                    <p className="text-lg"><strong>Localização (X, Y): </strong>
                                                         ({pedido.localizacao.x}, {pedido.localizacao.y})
                                                     </p>
                                                     <p className="text-lg"><strong>Prioridade: </strong>{pedido.prioridade}</p>
