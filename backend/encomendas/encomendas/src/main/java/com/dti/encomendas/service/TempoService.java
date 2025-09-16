b9aeb840685b0b06b96d34fddabc88593f9171f4
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-09-16 00:08:33 -0300

    fix: fixing the lazy error
    no cenário descrito, eu tive que pegar a lista de pedidos direto do repositório através do id do drone por causa do erro do LAZY do Hibernate. Por padrão do @OneToMany, a lista so será carregada em um outro momento, quando a sessao do banco de dados esta ativa, porem como estamos utilizando uma notação @Async, a sessão já está fechada, pois estamos em uma outra thread.

--- backend/encomendas/encomendas/src/main/java/com/dti/encomendas/service/TempoService.java
+++ backend/encomendas/encomendas/src/main/java/com/dti/encomendas/service/TempoService.java
@@ -11,6 +11,7 @@ import com.dti.encomendas.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -42,7 +43,7 @@ public class TempoService {
                     .filter(pedido -> pedido.getStatusPedido().equals(StatusPedido.ENVIADO)).toList();
 
             if (!pedidosParaEntrega.isEmpty()) { //precisa adicionar a verificacao de status pro pedido;
-                System.out.println(pedidos.toString());
+                System.out.println(pedidos);
                 Entrega entrega = entregaService.criarEntrega(drone);
 
                 drone.setStatus(StatusDrone.EM_VOO);
@@ -53,29 +54,33 @@ public class TempoService {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
-                entregarPedido(drone);
-                finalizarVoo(drone, entrega);
+                entregarPedido(drone, entrega);
             }
 
         }
 
     }
 
-    private void entregarPedido(Drone drone) {
+    @Transactional
+    private void entregarPedido(Drone drone, Entrega entrega) {
         Drone droneAtualizado = droneRepository.findById(drone.getId()).orElse(drone); //getting the latest drone
 
         droneAtualizado.setStatus(StatusDrone.ENTREGANDO);
         droneRepository.save(droneAtualizado);
 
-        Pedido pedido = droneAtualizado.getPedidos().stream()
+        List<Pedido> pedidos = pedidoRepository.findByDrone_Id(drone.getId()); //fixing the lazy error
+        Pedido pedido = pedidos.stream()
                 .filter((p) -> p.getStatusPedido().equals(StatusPedido.ENVIADO))
                 .findFirst().
                 orElse(null);
 
         if (pedido != null) {
+            System.out.println("Antes de salvar pedido: Drone está = " + pedido.getDrone().getStatus());
+
             pedido.setStatusPedido(StatusPedido.ENTREGUE);
             pedidoRepository.save(pedido);
+            System.out.println("Depois de salvar pedido: Drone está = " + pedido.getDrone().getStatus());
+
         }
 
         try {
@@ -84,14 +89,18 @@ public class TempoService {
             Thread.currentThread().interrupt();
         }
 
+        finalizarVoo(drone, entrega);
     }
 
+    @Transactional
     private void finalizarVoo(Drone drone, Entrega entrega) {
-        Drone droneAtualizado = droneRepository.findById(drone.getId()).orElse(drone); //getting the latest drone
-
-        droneAtualizado.setStatus(StatusDrone.IDLE);
         entregaService.finalizarEntrega(entrega);
-        droneRepository.save(droneAtualizado);
+
+        Drone droneAtualizado = droneRepository.findById(drone.getId()).orElse(drone);
+        droneAtualizado.setStatus(StatusDrone.IDLE);
+
+        System.out.println("Salvando drone como IDLE: " + droneAtualizado.getId());
+        droneRepository.saveAndFlush(droneAtualizado);
     }
 
 }
